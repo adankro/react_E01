fd2c4c75ef958ce8d81dfc1e93c131ef59588cde
Adan Rosales <adan.rosales_c@janrain.com>
2019-08-01 11:49:47 -0500

    changes and test

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+const label_1 ="First Name:"
+var button ={'text':'submmit'};
+
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
 
 //Create a react component
 const App = () => {
-    const buttonText ="Click me!"
+    
     return (
     <div>
-        <label className="label" for="name">Enter name:</label>
+        <label className="label" htmlFor="name">{label_1}</label>
         <input id="name" type="text" />
-        <button style={{ backgroundColor:'blue',color: 'white' }}>
-        {buttonText}
+        <button  style={{ backgroundColor:'blue',color: 'white' }}>
+        {button.text}
         </button>
+        <br/>
+        <label >{getTime()}</label>
     </div>
     );
 }
